e08a3cd6ac764650883022a22191011c4e5b671d
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-08-25 08:08:17 +0330

    Add login Screen Details

--- app/app/src/main/java/com/ahmad/aghazadeh/readerapp/screens/SplashScreen.kt
+++ app/app/src/main/java/com/ahmad/aghazadeh/readerapp/screens/SplashScreen.kt
@@ -1,7 +1,7 @@
 package com.ahmad.aghazadeh.readerapp.screens
 
 import android.view.animation.OvershootInterpolator
-import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
@@ -19,30 +19,37 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.ahmad.aghazadeh.readerapp.components.ReaderLogo
+import com.ahmad.aghazadeh.readerapp.navigation.ReaderScreens
 import com.ahmad.aghazadeh.readerapp.ui.AppColors
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
-fun SplashScreen(navController: NavController=NavController(context=LocalContext.current)) {
+fun SplashScreen(navController: NavController) {
 
-    val scale=remember{
-         Animatable(AppColors.red500)
+    val scale = remember{
+         Animatable(0f)
     }
     LaunchedEffect(key1 =true){
-        scale.animateTo(targetValue = 0.9f,
+        scale.animateTo(targetValue =1f,
             animationSpec = tween(durationMillis =800,
                 easing={
-                    OvershootInterpolator(8f)
+                    OvershootInterpolator(4f)
                         .getInterpolation(it)
                 }))
+        delay(2000L)
+        navController.navigate(ReaderScreens.LoginScreen.name)
     }
     Surface(modifier= Modifier
         .padding(16.dp)
-        .size(330.dp),
+        .size(250.dp)
+        .scale(scale.value),
         shape=CircleShape,
         color= AppColors.white,
         border = BorderStroke(width = 2.dp, color = AppColors.gray600)
@@ -50,8 +57,7 @@ fun SplashScreen(navController: NavController=NavController(context=LocalContext
         Column(modifier= Modifier.padding(1.dp),
             horizontalAlignment =Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center) {
-            Text(text = "Reader App",style =MaterialTheme.typography.displayMedium,
-                color = AppColors.red900.copy(alpha =0.5f))
+            ReaderLogo()
             Spacer(modifier= Modifier.height(16.dp))
             Text(text = "\"Read. Change. Yourself\"",style =MaterialTheme.typography.bodyLarge,
                 color = AppColors.gray700.copy(alpha =0.5f))
--- app/app/src/main/java/com/ahmad/aghazadeh/readerapp/screens/login/LoginScreen.kt
+++ app/app/src/main/java/com/ahmad/aghazadeh/readerapp/screens/login/LoginScreen.kt
@@ -1,10 +1,12 @@
 package com.ahmad.aghazadeh.readerapp.screens.login
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.ahmad.aghazadeh.readerapp.components.ReaderLogo
 
 @Composable
 fun LoginScreen(navController: NavController) {
-
+    ReaderLogo()
 }
 
